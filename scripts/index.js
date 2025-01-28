7d13647553cfa658ffb1f8cfe6b8377b2a5f70eb
William Marlette <williammarlette3@gmail.com>
2025-01-28 06:08:35 -0500

    add basic open and close functionality to edit profile modal

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,16 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
+
+const profileEditButton = document.querySelector(".profile__edit-btn");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const closeProfileModal = editProfileModal.querySelector(".modal__close-btn");
+
+function openModal() {
+  editProfileModal.classList.add("modal_opened");
+}
+function closeModal() {
+  editProfileModal.classList.remove("modal_opened");
+}
+profileEditButton.addEventListener("click", openModal);
+closeProfileModal.addEventListener("click", closeModal);
